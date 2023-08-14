c1938c2fca58ebce19c10768bd5ae39e81a06845
V-Sergio-S <mrcassicman@mail.ru>
2023-08-14 10:38:56 +0300

    change scss files

--- src/assets/js/mobile-nav.js
+++ src/assets/js/mobile-nav.js
@@ -1,50 +1,59 @@
 
-const burger = document.querySelector('[data-burger]');
-const sidebar = document.getElementById('sidebar');
-const scroll = document.querySelectorAll('[data-scroll]');
-const body = document.body;
+(function () {
 
-burger.addEventListener('click', event =>{
-    if (body.classList.contains('show-sidebar')){
-        closeSidebar();
+    const burger = document.querySelectorAll('[data-burger]');
+    const sidebar = document.getElementById('sidebar');
+    const scroll = document.querySelectorAll('[data-scroll]');
+    const body = document.body;
+
+
+    for (let i = 0; i < burger.length; i++) {
+        const burgerId = burger[i];
+        burgerId.addEventListener('click', event =>{
+            if (body.classList.contains('show-sidebar')){
+                closeSidebar();
+            }
+            else {
+                showsidebar();
+            }
+        });
+
     }
-    else {
-        showsidebar();
+
+    function showsidebar(){
+        let mask = document.createElement('div');
+        mask.classList.add('sidebar__mask');
+        mask.addEventListener('click', closeSidebar);
+        body.classList.add('show-sidebar');
     }
-});
 
-function showsidebar(){
-    let mask = document.createElement('div');
-    mask.classList.add('sidebar__mask');
-    mask.addEventListener('click', closeSidebar);
-    body.classList.add('show-sidebar');
-}
+    function closeSidebar(){
+        body.classList.remove('show-sidebar');
+        document.querySelector('.sidebar__mask').remove();
+    }
 
-function closeSidebar(){
-    body.classList.remove('show-sidebar');
-    document.querySelector('.sidebar__mask').remove();
-}
+    function scrollMenu (e) {
 
-function scrollMenu (e) {
+        let $this = e.currentTarget;
+        let scrollId = $this.getAttribute('data-scroll');
+        let scrollItem = document.getElementById(scrollId);
+        scrollItem.scrollIntoView({behavior: 'smooth'});
 
-    let $this = e.currentTarget;
-    let scrollId = $this.getAttribute('data-scroll');
-    let scrollItem = document.getElementById(scrollId);
-    scrollItem.scrollIntoView({behavior: 'smooth'});
+    }
 
-}
+    for (let i = 0; i < scroll.length; i++) {
+        const scrollId = scroll[i];
+        scrollId.addEventListener('click', (e)=>{
+            if (e.target) {
+                e.preventDefault();
+            }
+            scrollMenu (e)
 
-for (let i = 0; i < scroll.length; i++) {
-    const scrollId = scroll[i];
-    scrollId.addEventListener('click', (e)=>{
-        if (e.target) {
-            e.preventDefault();
-        }
-        scrollMenu (e)
+            if (body.classList.contains('show-sidebar')){
+                closeSidebar();
+            }
 
-        if (body.classList.contains('show-sidebar')){
-            closeSidebar();
-        }
+        })
+    }
 
-    })
-}
+})();
--- src/assets/js/tabs.js
+++ src/assets/js/tabs.js
@@ -1,5 +1,6 @@
+(function () {
 
-const tabsButton = document.querySelectorAll('[data-tabs]')
+    const tabsButton = document.querySelectorAll('[data-tabs]')
 
     tabsButton.forEach(button => {
 
@@ -7,12 +8,11 @@ const tabsButton = document.querySelectorAll('[data-tabs]')
             e.stopPropagation()
 
             let buttonCurrent = e.currentTarget
-            console.log(buttonCurrent)
             let currentIdElement = buttonCurrent.getAttribute('data-tabs')
-            console.log(currentIdElement)
             let tabsInfo = document.getElementById(currentIdElement)
             tabsInfo.classList.toggle('always-question__info--block')
             button.classList.toggle('always-question__button--active')
         }
 
     })
+})();
